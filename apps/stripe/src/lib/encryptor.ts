fb9ce9c5ca7992b8751966e1a0812c3ca6e7324a
momina-bs <momina.byteshifted@gmail.com>
2025-07-07 04:17:52 +0500

    Update encryptor.ts

--- apps/stripe/src/lib/encryptor.ts
+++ apps/stripe/src/lib/encryptor.ts
@@ -10,7 +10,7 @@ export class Encryptor {
   private secret: string;
 
   constructor(secret = env.SECRET_KEY) {
-    this.secret = secret ?? "yjpFOazOOA0/tf+m+eS+t9KhGGuBLyLqZkTp2pSMF14piO3NpIla6bOKvqjQQLwr";
+    this.secret = secret ?? "ulnbgtmje67/Tf5NrLJwQreK1hba6PDFHcPZIWFuwaY=";
   }
 
   encrypt(text: string): string {
